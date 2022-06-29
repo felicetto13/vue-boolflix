0f51aa7380708d89385c3bfd7ca0a27f09f3efd5
felicetto13 <felice.laterza93@icloud.com>
2022-06-29 12:31:34 +0200

    added hover effect on the card

--- src/components/CardMovies.vue
+++ src/components/CardMovies.vue
@@ -1,43 +1,42 @@
 <template>
-    <div class="card">
-        <div class="img-card">
-          <img
-            :src="'https://image.tmdb.org/t/p/w342' + film.poster_path"
-            :alt="film.original_title"
-            @error="setAltImg"
-          />
-        </div>
-        <div class="card-description">
-          <span>
-            <strong>Titolo originale: </strong>{{ film.original_title }}
-          </span>
-          <span> <strong>Titolo: </strong>{{ film.title }} </span>
-          <span>
-            <strong>Lingua Originale: </strong>{{ film.original_language }}
-          </span>
-          <span>
-            <strong>Voto: </strong>
-            <span>
-              <i v-for="(number, z) in 5" :key="z" class="fa-regular fa-star"></i>
-              <i
-                v-for="(number, y) in getStar(film.vote_average)"
-                :key="y"
-                class="fa-solid fa-star"
-              ></i>
-            </span>
-          </span>
-        </div>
+  <div class="card">
+    <div class="img-card">
+      <img
+        :src="'https://image.tmdb.org/t/p/w342' + film.poster_path"
+        :alt="film.original_title"
+        @error="setAltImg"
+      />
     </div>
+    <div class="card-description">
+      <span>
+        <strong>Titolo originale: </strong>{{ film.original_title }}
+      </span>
+      <span> <strong>Titolo: </strong>{{ film.title }} </span>
+      <span>
+        <strong>Lingua Originale: </strong>{{ film.original_language }}
+      </span>
+      <span>
+        <strong>Voto: </strong>
+        <span>
+          <i v-for="(number, z) in 5" :key="z" class="fa-regular fa-star"></i>
+          <i
+            v-for="(number, y) in getStar(film.vote_average)"
+            :key="y"
+            class="fa-solid fa-star"
+          ></i>
+        </span>
+      </span>
+      <span><strong>Descrizione: </strong> {{ film.overview }}</span>
+    </div>
+  </div>
 </template>
 
 <script>
-
-
 export default {
-  props:{
-      film: Object
+  props: {
+    film: Object,
   },
-   methods: {
+  methods: {
     getStar(vote) {
       debugger;
       return Math.ceil(vote / 2);
@@ -47,25 +46,42 @@ export default {
       event.target.class = "resize-img";
     },
   },
-  
 };
 </script>
 
 <style lang="scss" scoped>
 .img-card {
-    img {
-      object-fit: cover;
-      aspect-ratio: 1/1.5;
-      width: 350px;
-    }
+  img {
+    object-fit: cover;
+    aspect-ratio: 1/1.5;
+    width: 350px;
   }
+}
 
-  i.fa-solid.fa-star {
-    transform: translateX(-500%);
-  }
+i.fa-solid.fa-star {
+  transform: translateX(-500%);
+  color: gold;
+}
 
 .card-description {
   display: flex;
   flex-direction: column;
+  position: absolute;
+  padding: 2rem 1rem;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.7);
+  transform: rotateY(-90deg);
+  transition: transform .4s ease-in-out;
+  color: white;
+
+    span{
+        margin-bottom: 10px;
+    }
+}
+.card:hover .card-description{
+    transform: rotateY(0deg);
 }
 </style>
--- src/components/CardSeries.vue
+++ src/components/CardSeries.vue
@@ -1,46 +1,40 @@
 <template>
-    <div class="card">
-        
-            <div class="img-card">
-              <img
-                :src="'https://image.tmdb.org/t/p/w342' + serie.poster_path"
-                :alt="serie.original_title"
-                @error="setAltImg"
-              />
-            </div>
-            <div class="card-description">
-              <span>
-                <strong>Titolo originale: </strong>{{ serie.original_name }}
-              </span>
-              <span> <strong>Titolo: </strong>{{ serie.name }} </span>
-              <span>
-                <strong>Lingua Originale: </strong>{{ serie.original_language }}
-              </span>
-              <span>
-                <strong>Voto: </strong>
-                <span>
-                  <i
-                    v-for="(number, z) in 5"
-                    :key="z"
-                    class="fa-regular fa-star"
-                  ></i>
-                  <i
-                    v-for="(number, y) in getStar(serie.vote_average)"
-                    :key="y"
-                    class="fa-solid fa-star"
-                  ></i>
-                </span>
-              </span>
-            </div>
+  <div class="card">
+    <div class="img-card">
+      <img
+        :src="'https://image.tmdb.org/t/p/w342' + serie.poster_path"
+        :alt="serie.original_title"
+        @error="setAltImg"
+      />
     </div>
+    <div class="card-description">
+      <span>
+        <strong>Titolo originale: </strong>{{ serie.original_name }}
+      </span>
+      <span> <strong>Titolo: </strong>{{ serie.name }} </span>
+      <span>
+        <strong>Lingua Originale: </strong>{{ serie.original_language }}
+      </span>
+      <span>
+        <strong>Voto: </strong>
+        <span>
+          <i v-for="(number, z) in 5" :key="z" class="fa-regular fa-star"></i>
+          <i
+            v-for="(number, y) in getStar(serie.vote_average)"
+            :key="y"
+            class="fa-solid fa-star"
+          ></i>
+        </span>
+      </span>
+    </div>
+  </div>
 </template>
 
 <script>
-
 export default {
-    props:{
-        serie: Object
-    },
+  props: {
+    serie: Object,
+  },
   methods: {
     getStar(vote) {
       debugger;
@@ -51,24 +45,38 @@ export default {
       event.target.class = "resize-img";
     },
   },
-}
+};
 </script>
 
 <style lang="scss" scoped>
 .img-card {
-    img {
-      object-fit: cover;
-      aspect-ratio: 1/1.5;
-      width: 350px;
-    }
+  img {
+    object-fit: cover;
+    aspect-ratio: 1/1.5;
+    width: 350px;
   }
+}
 
-  i.fa-solid.fa-star {
-    transform: translateX(-500%);
-  }
+i.fa-solid.fa-star {
+  transform: translateX(-500%);
+  color: gold;
+}
 
 .card-description {
   display: flex;
   flex-direction: column;
+  position: absolute;
+  padding: 2rem 1rem;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.7);
+  transform: rotateY(-90deg);
+  transition: transform .4s ease-in-out;
+  color: white;
+}
+.card:hover .card-description{
+    transform: rotateY(0deg);
 }
 </style>
