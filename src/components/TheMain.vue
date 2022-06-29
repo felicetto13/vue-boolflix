bf6ef3f6affca86876ce3d54fd8fc5656c0a9f90
felicetto13 <felice.laterza93@icloud.com>
2022-06-29 03:20:47 +0200

    half milestone 2 completed

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -1,23 +1,45 @@
 <template>
     <div class="container text-center">
-        <ul class="list-unstyled" v-for="(film,i) in fetchDataOn" :key="i">
+        <ul>
+            <li v-for="lang in getLanguage" :key="lang">
+            {{ lang }}</li>
+        </ul>
+        <ul class="list-unstyled" v-for="(film,i) in fetchDataMovies" :key="i">
             <li><strong>Titolo originale: </strong>{{ film.original_title }}</li>
             <li><strong>Titolo: </strong>{{ film.title }}</li>
             <li><strong>Lingua Originale: </strong>{{ film.original_language }}</li>
             <li><strong>Voto: </strong>{{ film.vote_average }}</li>
+        </ul>
+
+        <ul class="list-unstyled" v-for="(serie) in fetchDataSeries" :key="serie">
+            <li><strong>Titolo originale: </strong>{{ serie.original_name }}</li>
+            <li><strong>Titolo: </strong>{{ serie.name }}</li>
+            <li><strong>Lingua Originale: </strong>{{ serie.original_language }}</li>
+            <li><strong>Voto: </strong>{{ serie.vote_average }}</li>
         </ul>
     </div>
 </template>
 
 <script>
 import {state} from "../store"
+import {fetchData} from "../store"
 export default {
     computed:
     {
-        fetchDataOn(){
+        fetchDataMovies(){
 
             return state.moviesList;
+        },
+        getLanguage(){
+            return state.languageMovies
+        },
+        fetchDataSeries(){
+            return state.seriesList;
         }
+        
+    },
+    mounted(){
+        fetchData();
     }
 }
 </script>
--- src/store.js
+++ src/store.js
@@ -3,18 +3,49 @@ import axios from "axios";
 
 export const state = Vue.observable({
     moviesList: [],
-    seriesList: []
+    languageMovies: [],
+    languageSeries: [],
+    seriesList: [],
 })
 
-export function fetchData (textQuery){
-    axios.get("https://api.themoviedb.org/3/search/movie",{
+export function fetchData (/* textQuery */){
+    axios.get("https://api.themoviedb.org/3/discover/movie",{
         params:{
             api_key:"d206597de1040586ba02dfac9aaa6cff",
-            query:textQuery,
-            language: "it-IT",
+            /* query:textQuery, */
+            /* language: "it-IT", */
         }
     })
     .then((resp) => {
+        debugger
         state.moviesList = resp.data.results;
+        for(let i =0; i < state.moviesList.length ; i++){
+            debugger
+            console.log(state.moviesList[i].original_language);
+            if(!state.languageMovies.includes(state.moviesList[i].original_language)){
+                state.languageMovies.push(state.moviesList[i].original_language)
+            }
+        }
+       
     })
+    axios.get("https://api.themoviedb.org/3/discover/tv",{
+        params:{
+            api_key:"d206597de1040586ba02dfac9aaa6cff",
+            /* query:textQuery, */
+            /* language: "it-IT", */
+        }
+    })
+    .then((resp) => {
+        debugger
+        state.seriesList = resp.data.results;
+        for(let i =0; i < state.seriesList.length ; i++){
+            debugger
+            console.log(state.seriesList[i].original_language);
+            if(!state.languageSeries.includes(state.seriesList[i].original_language)){
+                state.languageSeries.push(state.seriesList[i].original_language)
+            }
+        }
+       
+    })
+
 }
